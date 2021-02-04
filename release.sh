c89564cc2288ccedd571e1b304c3e0824b27ee11
Gustavo Bicalho <gusbicalho@gmail.com>
2021-02-04 09:18:52 -0300

    improve release script

--- release.sh
+++ release.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 
 cabal build
 [ -d dist ] || mkdir dist
-zip dist/uncertain-gantt.x86_64-osx.zip \
-  dist-newstyle/build/x86_64-osx/ghc-8.10.3/uncertain-gantt-0.0.0.0/x/uncertain-gantt/build/uncertain-gantt/uncertain-gantt \
-  resources/example.ug
+cp dist-newstyle/build/x86_64-osx/ghc-8.10.3/uncertain-gantt-0.0.0.0/x/uncertain-gantt/build/uncertain-gantt/uncertain-gantt dist/
+cp resources/example.ug dist/
+cd dist
+[ -f uncertain-gantt.x86_64-osx.zip ] && rm uncertain-gantt.x86_64-osx.zip || true
+zip uncertain-gantt.x86_64-osx.zip uncertain-gantt example.ug
